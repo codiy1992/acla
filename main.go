5dac1f694edb32f907dc5052bc79473ff1fe121e
codiy <mail@codiy.net>
2022-03-17 14:00:00 +0800

    fix

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 var flagDeckName = flag.String("deck", "current", "deck name")
 var flagModelName = flag.String("model", "", "model name(note type)")
 var flagFieldName = flag.String("field", "", "filed name")
-var flagFieldValue = flag.String("vaule", "", "filed value")
+var flagFieldValue = flag.String("value", "", "filed value")
 var flagQuery = flag.String("query", "", "query string")
 var flagTags = flag.String("tags", "", "tags")
 
